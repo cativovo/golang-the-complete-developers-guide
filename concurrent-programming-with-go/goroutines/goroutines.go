7691200c0d1df2ae3810fdcbf58e9f0ed72741e4
Mark Leo Cativo <cativomarkleo@gmail.com>
2023-09-27 16:51:39 +0800

    add goroutines exercise

---
+++ concurrent-programming-with-go/goroutines/goroutines.go
@@ -0,0 +1,83 @@
+//--Summary:
+//  Create a program to read a list of numbers from multiple files,
+//  sum the total of each file, then sum all the totals.
+//
+//--Requirements:
+//* Sum the numbers in each file noted in the main() function
+//* Add each sum together to get a grand total for all files
+//  - Print the grand total to the terminal
+//* Launch a goroutine for each file
+//* Report any errors to the terminal
+//
+//--Notes:
+//* This program will need to be ran from the `lectures/exercise/goroutines`
+//  directory:
+//    cd lectures/exercise/goroutines
+//    go run goroutines
+//* The grand total for the files is 4103109
+//* The data files intentionally contain invalid entries
+//* stdlib packages that will come in handy:
+//  - strconv: parse the numbers into integers
+//  - bufio: read each line in a file
+//  - os: open files
+//  - io: io.EOF will indicate the end of a file
+//  - time: pause the program to wait for the goroutines to finish
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"time"
+)
+
+func sumFile(reader *bufio.Reader) int {
+	sum := 0
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		if err == io.EOF {
+			return sum
+		}
+
+		num, err := strconv.Atoi(line[:len(line)-1])
+
+		if err != nil {
+			fmt.Println("Error:", err)
+		} else {
+			sum += num
+		}
+	}
+
+}
+
+func main() {
+	files := []string{"num1.txt", "num2.txt", "num3.txt", "num4.txt", "num5.txt"}
+	sum := 0
+
+	for _, file := range files {
+		file, err := os.Open(file)
+
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		reader := bufio.NewReader(file)
+
+		calculate := func() {
+			filesum := sumFile(reader)
+			sum += filesum
+		}
+
+		go calculate()
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	fmt.Println("Sum of files:", sum)
+}
